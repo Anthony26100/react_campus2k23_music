047b5c65b69ef98a0814a56b9b646462dee6b800
Anthony <anthony.bellier@laposte.net>
2023-06-01 23:57:25 +0200

    (feat) test differents method for persist data

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { Link, Route } from "wouter";
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import AlbumLists from "./components/AlbumLists";
+import { Routes, Route } from "react-router-dom";
 import AlbumDetails from "./components/AlbumDetails";
 
 function App() {
+  const [detailsAlbum, setDetailsAlbum] = useState<string[]>([]);
+
   return (
     <div className="App">
       <Navbar />
       <div>
-        <AlbumLists />
-
-        <AlbumDetails />
+        <Routes>
+          <Route path="/" element={<AlbumLists />} />
+          <Route path="/:id" element={<AlbumDetails />} />
+        </Routes>
       </div>
     </div>
   );
--- src/components/AlbumDetails.tsx
+++ src/components/AlbumDetails.tsx
@@ -1,8 +1,15 @@
 import React from "react";
-import Music from "../api/albums.json";
+import { useParams } from "react-router-dom";
+
+// interface Props {
+//   music: string;
+//   setMusic: React.Dispatch<React.SetStateAction<string[]>>;
+// }
 
 function AlbumDetails() {
-  return <>bonjour details albums</>;
+  // const { music, setMusic } = props;
+  const { id } = useParams();
+  return <>bonjour details albums {id}</>;
 }
 
 export default AlbumDetails;
